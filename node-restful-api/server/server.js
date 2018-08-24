630cd8f7abe802a9c2c5ec08b08f7aa0a6e0a2d1
Jay <jay@ajiede-MacBook-Pro.local>
2018-08-24 21:59:14 +0800

    Add DELETE /users/me/token route

--- node-restful-api/server/server.js
+++ node-restful-api/server/server.js
@@ -140,6 +140,16 @@ app.post('/users/login', (req, res) => {
     })
 })
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token)
+    .then(() => {
+      res.send();
+    })
+    .catch((e) => {
+      res.status(400).send();
+    })
+})
+
 app.listen(process.env.PORT, () => {
   console.log('Server is started')
 })
