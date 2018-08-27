801f234402775d1fef0de09b06b6ecdadc7b7ff1
JayZang <sa841218as@gmail.com>
2018-08-27 17:41:14 +0800

    Use Async/await function

--- node-restful-api/server/server.js
+++ node-restful-api/server/server.js
@@ -66,30 +66,44 @@ app.get('/todo/:id', authenticate, (req, res) => {
     })
 })
 
-app.delete('/todo/:id', authenticate, (req, res) => {
+app.delete('/todo/:id', authenticate, async (req, res) => {
   var id = req.params.id;
 
   if(!ObjectID.isValid(id))
     return res.status(404).send();
 
-  Todo.findOneAndRemove({
-      _id: id,
-      _creator: req.user._id
-    })
-    .then((todo) => {
-      if(!todo)
-        res.status(404).send();
+  try {
+    var todo = await Todo.findOneAndRemove({
+        _id: id,
+        _creator: req.user._id
+      })
 
-      res.send({todo});
-    })
-    .catch((e) => {
-      res.status(404).send();
-    })
+    if(!todo)
+      return res.status(404).send();
+
+    res.send({todo});
+  } catch (e) {
+    res.status(404).send();
+  }
+
+  // Todo.findOneAndRemove({
+  //     _id: id,
+  //     _creator: req.user._id
+  //   })
+  //   .then((todo) => {
+  //     if(!todo)
+  //       res.status(404).send();
+  //
+  //     res.send({todo});
+  //   })
+  //   .catch((e) => {
+  //     res.status(404).send();
+  //   })
 })
 
 // Patch 為 Http 協議中的更新 Request（資料原本就有，但 '部份' 更新）
 // 而 PUT Request 則是假使原有資料就更換成新數據（有替換之意），若沒有則新增數據
-app.patch('/todo/:id', authenticate, (req, res) => {
+app.patch('/todo/:id', authenticate, async (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
 
@@ -103,20 +117,34 @@ app.patch('/todo/:id', authenticate, (req, res) => {
     body.completeAt = null;
   }
 
-  Todo.findOneAndUpdate({
-      _id: id,
-      _creator: req.user._id
-    }, {$set: body}, {new: true})
-    .then((todo) => {
-      if(!todo)
-        return res.status(404).send();
+  try {
+    var todo = await Todo.findOneAndUpdate({
+        _id: id,
+        _creator: req.user._id
+      }, {$set: body}, {new: true})
 
-      res.send({todo});
-    })
-    .catch((e) => {
-      console.log(e)
-      res.status(404).send(e);
-    })
+    if(!todo)
+      return res.status(404).send()
+
+    res.send({todo});
+  } catch (e) {
+    res.status(404).send()
+  }
+
+  // Todo.findOneAndUpdate({
+  //     _id: id,
+  //     _creator: req.user._id
+  //   }, {$set: body}, {new: true})
+  //   .then((todo) => {
+  //     if(!todo)
+  //       return res.status(404).send();
+  //
+  //     res.send({todo});
+  //   })
+  //   .catch((e) => {
+  //     console.log(e)
+  //     res.status(404).send(e);
+  //   })
 })
 
 app.get('/users/me', authenticate, (req, res) => {
@@ -124,44 +152,60 @@ app.get('/users/me', authenticate, (req, res) => {
 })
 
 // regist a aser
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body)
 
-  user.save()
-    .then(() => {
-      return user.generateAuthToken()
-    })
-    .then((token) => {
-      res.header('x-auth', token).send(user);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    })
+  try {
+    await user.save();
+    var token = await user.generateAuthToken();
+    res.header('x-auth', token).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+
+  // user.save()
+  //   .then(() => {
+  //     return user.generateAuthToken()
+  //   })
+  //   .then((token) => {
+  //     res.header('x-auth', token).send(user);
+  //   })
+  //   .catch((e) => {
+  //     res.status(400).send(e);
+  //   })
 })
 
-app.post('/users/login', (req, res) => {
+app.post('/users/login', async (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
-  User.findByCredentials(body.email, body.password)
-    .then((user) => {
-      return user.generateAuthToken()
-        .then((token) => {
-          res.header('x-auth', token).send(user);
-        })
-    })
-    .catch((e) => {
-      res.status(400).send();
-    })
+
+  try {
+    var user = await User.findByCredentials(body.email, body.password);
+    var token = await user.generateAuthToken();
+    res.header('x-auth', token).send(user);
+  } catch (e) {
+    res.status(400).send();
+  }
+
+  // User.findByCredentials(body.email, body.password)
+  //   .then((user) => {
+  //     return user.generateAuthToken()
+  //       .then((token) => {
+  //         res.header('x-auth', token).send(user);
+  //       })
+  //   })
+  //   .catch((e) => {
+  //     res.status(400).send();
+  //   })
 })
 
-app.delete('/users/me/token', authenticate, (req, res) => {
-  req.user.removeToken(req.token)
-    .then(() => {
-      res.send();
-    })
-    .catch((e) => {
-      res.status(400).send();
-    })
+app.delete('/users/me/token', authenticate, async (req, res) => {
+  try {
+    await req.user.removeToken(req.token);
+    res.send();
+  } catch (e) {
+    res.status(400).send();
+  }
 })
 
 app.listen(process.env.PORT, () => {
